4387928eafafde977f6d162feb7be91ed8f8273d
Allan Odreman <43389674+mrallanodreman@users.noreply.github.com>
2026-08-19 19:18:18 -0500

    feat(v0.9): add backend package

---
+++ knob_controller/backends/__init__.py
@@ -0,0 +1,9 @@
+from .linux import LinuxActionExecutor, LinuxKeyMap, ModifierDeviceSet, ModifierState, discover_modifier_devices
+
+__all__ = [
+    "LinuxActionExecutor",
+    "LinuxKeyMap",
+    "ModifierDeviceSet",
+    "ModifierState",
+    "discover_modifier_devices",
+]
